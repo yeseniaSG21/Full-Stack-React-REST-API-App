2410566357881eb24653c0add6915d6675ccaf59
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-20 00:22:53 -0800

    Create ErrorsDisplay function for the Form

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -22,31 +22,32 @@ export default (props) => {
 
     return (
         <div>
-        <ErrorsDisplay errors={errors} />
-        <form onSubmit={handleSubmit}>
-            {elements()}
-            <div className="pad-bottom">
-            <button className="button" type="submit">{submitButtonText}</button>
-            <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
-            </div>
-        </form>
+            <ErrorsDisplay errors={errors} />
+            <form onSubmit={handleSubmit}>
+                {elements()}
+                <div className="pad-bottom">
+                    <button className="button" type="submit">{submitButtonText}</button>
+                    <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
+                </div>
+            </form>
         </div>
     );
 }
 
-    function ErrorsDisplay({ errors }) {
+// Creates the Validation Errors to use in the Form above
+function ErrorsDisplay({ errors }) {
     let errorsDisplay = null;
 
     if (errors.length) {
         errorsDisplay = (
-        <div>
-            <h2 className="validation--errors--label">Validation errors</h2>
-            <div className="validation-errors">
-            <ul>
-                {errors.map((error, i) => <li key={i}>{error}</li>)}
-            </ul>
+            <div>
+                <h2 className="validation--errors--label">Validation errors</h2>
+                <div className="validation-errors">
+                    <ul>
+                        {errors.map((error, i) => <li key={i}>{error}</li>)}
+                    </ul>
+                </div>
             </div>
-        </div>
         );
     }
 
