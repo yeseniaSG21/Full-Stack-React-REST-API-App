907002f7115abce29a38943a86819de504c7e6b5
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-20 15:25:42 -0800

    Add Courses component comments

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+/**
+ * This component provides the "Courses" screen by:
+    ** Retrieving the list of courses from the REST API's /api/courses route
+    ** Rendering a list of courses. Each course needs to link to its respective "Course Detail" screen. 
+    ** Rendering a link to the "Create Course" screen.
+ **/
+
+
+
 export default function Courses() {
     return (
         <div>Courses</div>
