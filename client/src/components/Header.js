31f2be9d3d5f61f537751b866537144c3c5f7ad6
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-19 16:19:20 -0800

    Add Header component comments

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Header() {
-    return (
-        <div>Header</div>
-    )
-}
+/**
+ * This component displays the top menu bar for the application:
+    ** Includes buttons for signing in and signing up (if there's not an authenticated user).
+    ** Or the user's name and a button for signing out (if there's an authenticated user).
+**/
 
-export default Header;
+export default class Header extends React.PureComponent {
+    render() {
+        const { context } = this.props;
+        const authUser = context.authenticatedUser;
+        return (
+            <div className="header">
+            <div className="bounds">
+                <h1 className="header--logo">MyAuth</h1>
+                <nav>
+                {authUser ? (       // Conditionally Render the Header Nav with authUser as the condition
+                    <React.Fragment>      // if true
+                    <span>Welcome, {authUser.name}!</span>
+                    <Link to="/signout">Sign Out</Link>
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>    // if false
+                    <Link className="signup" to="/signup">Sign Up</Link>
+                    <Link className="signin" to="/signin">Sign In</Link>
+                    </React.Fragment>
+                )}
+                </nav>
+            </div>
+            </div>
+        );
+    }
+};
