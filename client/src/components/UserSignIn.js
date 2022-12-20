85414feee64ae5d9cf8237676e83099032466995
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-20 00:34:36 -0800

    Add comments to UserSignIn component

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+/**
+ * This component provides the "Sign In" screen by:
+    ** Rendering a form that allows a user to sign in using their existing account information. 
+    ** Rendering a "Sign In" button that when clicked signs in the user.
+    ** Rendering a "Cancel" button that returns the user to the default route (i.e. the list of courses).
+**/
+
+
 function UserSignIn() {
     return (
         <div>UserSignIn</div>
