2a0e8dbe1be99107cfc116abf4b2c94c5729c7cf
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-20 00:15:29 -0800

    Create Form component

---
+++ client/src/components/Form.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+/**
+ * This component provides the "Form" utilized in user sign-in and sign-up by:
+    ** Anazlying the submit and cancel events.
+    ** Providing the submit and cancel buttons.
+    ** Rendering Validation errors.
+ **/
+
+export default (props) => {
+    const { cancel, errors, submit, submitButtonText, elements } = props;
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        submit();
+    }
+
+    function handleCancel(event) {
+        event.preventDefault();
+        cancel();
+    }
+
+    return (
+        <div>
+        <ErrorsDisplay errors={errors} />
+        <form onSubmit={handleSubmit}>
+            {elements()}
+            <div className="pad-bottom">
+            <button className="button" type="submit">{submitButtonText}</button>
+            <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
+            </div>
+        </form>
+        </div>
+    );
+}
+
+    function ErrorsDisplay({ errors }) {
+    let errorsDisplay = null;
+
+    if (errors.length) {
+        errorsDisplay = (
+        <div>
+            <h2 className="validation--errors--label">Validation errors</h2>
+            <div className="validation-errors">
+            <ul>
+                {errors.map((error, i) => <li key={i}>{error}</li>)}
+            </ul>
+            </div>
+        </div>
+        );
+    }
+
+    return errorsDisplay;
+}
