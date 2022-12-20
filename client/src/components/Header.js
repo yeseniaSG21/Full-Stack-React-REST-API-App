ddabdc9c5a030e9084ea9c121ecd435604035e40
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-19 16:55:02 -0800

    Create Header component with routes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,23 +12,27 @@ export default class Header extends React.PureComponent {
         const { context } = this.props;
         const authUser = context.authenticatedUser;
         return (
-            <div className="header">
-                <div className="bounds">
-                    <h1 className="header--logo">MyAuth</h1>
-                    <nav>
-                        {authUser ? (       // Conditionally Render the Header Nav with authUser as the condition
-                            <React.Fragment>      
-                                <span>Welcome, {authUser.name}!</span>      
-                                <Link to="/signout">Sign Out</Link>         
-                            </React.Fragment>
-                        ) : (
-                            <React.Fragment>    
-                                <Link className="signup" to="/signup">Sign Up</Link>
-                                <Link className="signin" to="/signin">Sign In</Link>
-                            </React.Fragment>
-                        )}
-                    </nav>
-                </div>
+            <div className="wrap header--flex">
+                <h1 className="header--logo">
+                    <Link to='/'>Courses</Link>
+                </h1>
+                <nav>
+                    {authUser ? (       // Conditionally Render the Header Nav with authUser as the condition
+                        <React.Fragment>
+                            <ul className='header--signedin'>
+                                <li>Welcome, {authUser.firstName} {authUser.lastName}!</li>
+                                <li><Link to="/signout">Sign Out</Link></li>
+                            </ul>  
+                        </React.Fragment>
+                    ) : (
+                        <React.Fragment>
+                            <ul className='header--signedout'>
+                                <li><Link className="signup" to="/signup">Sign Up</Link></li>
+                                <li><Link className="signin" to="/signin">Sign In</Link></li>
+                            </ul>
+                        </React.Fragment>
+                    )}
+                </nav>
             </div>
         );
     }
