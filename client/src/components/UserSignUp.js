306f9bba050881ba483db2ae33cc44b7683bc240
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-19 16:58:48 -0800

    Add UserSignUp comments

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+/**
+ * This component provides the "Sign Up" screen by:
+    ** Rendering a form that allows a user to sign up by creating a new account.
+    ** Rendering a "Sign Up" button that when clicked sends a POST request to the REST API's /api/users route and signs in the user. 
+    ** Rendering a "Cancel" button that returns the user to the default route (i.e. the list of courses).
+**/
+
 function UserSignUp() {
     return (
         <div>UserSignUp</div>
