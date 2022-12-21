0a30b0d4a3eabb6e8fbecc739f0d1e12653da1c1
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-20 16:05:33 -0800

    Add comments to UpdateCourse component

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+/**
+ * This component provides the "Update Course" screen by:
+    ** Rendering a form that allows a user to update one of their existing courses. 
+    ** Rendering an "Update Course" button that when clicked sends a PUT request to the REST API's /api/courses/:id route. 
+    ** Rendering a "Cancel" button that returns the user to the "Course Detail" screen.
+**/
+
+
 function UpdateCourse() {
     return (
         <div>UpdateCourse</div>
