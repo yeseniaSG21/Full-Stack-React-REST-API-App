f22f49678dffe2e7e1dddecd467c7059d0b86360
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-20 16:03:24 -0800

    Add CreateCourse component comments

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+/**
+ * This component provides the "Create Course" screen by
+    ** Rendering a form that allows a user to create a new course.
+    ** Rendering a "Create Course" button that when clicked sends a POST request to the REST API's /api/courses route. 
+    ** Rendering a "Cancel" button that returns the user to the default route (i.e. the list of courses).
+**/
+
+
 function CreateCourse() {
     return (
         <div>CreateCourse</div>
