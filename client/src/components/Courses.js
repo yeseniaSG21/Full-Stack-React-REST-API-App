cdf9765e3d9211902055c686657ce807f3fa98af
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-21 19:34:02 -0800

    Construct Courses component

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 /**
  * This component provides the "Courses" screen by:
@@ -9,10 +8,38 @@ import axios from 'axios';
     ** Rendering a link to the "Create Course" screen.
  **/
 
-export default class Courses extends Component {
+export default function Courses({ context }) {
+    const [courses, setCourses] = useState([]);
 
+    useEffect(() => {
+        context.data 
+            .getCourses()
+            .then(data => setCourses(data))
+            .catch(error => console.log('Error fetching and parsing data', error));
+    }, [context.data]);
 
     return (
-        
-    )
+        <div className="wrap main--grid">
+            {courses.map((course, index) => (
+                <Link className="course--module course--link"  to={`/courses/${courses.id}`} key={index}>
+                    <h2 className="course--label">Course</h2>
+                    <h3 className="course--title">{course.title}</h3>
+                </Link>
+            ))}
+            <Link to="/courses/create" className="course--module course--add--module">
+                <span className="course--add--title">
+                    <svg 
+                        version="1.1" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        x="0px" 
+                        y="0px" 
+                        viewBox="0 0 13 13" 
+                        className="add">
+                        <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                    </svg>
+                    New Course
+                </span>
+            </Link>
+        </div>
+    );
 }
