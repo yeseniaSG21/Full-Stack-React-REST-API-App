337f6ebeb465f177c90d2db3a395dd4059139a27
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-22 15:57:13 -0800

    Create PrivateRoute component

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,5 +1,30 @@
-
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { Consumer } from './Context';
 
 /**
- * 
+ * This high-order component will serve for any  proteced routes and make accessible to authenticated users only. 
+     ** If user is authenticated, the user will be continue to the specified private component.
+     ** If user is not authenticated, they will be redirected to the /signin route.
 **/
+
+export default ({ component: Component, ...rest }) => {
+    return (
+        <Consumer>
+            {context => (
+            <Route
+                {...rest}
+                render={props => context.authenticatedUser ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to={{
+                        pathname: '/signin',
+                        state: { from: props.location }
+                    }} />
+                )
+                }
+            />
+        )}
+        </Consumer>
+    );
+};
