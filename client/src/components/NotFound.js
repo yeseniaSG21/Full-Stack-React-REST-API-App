3111c1de2e8f48fa782859927fa943f7cebebefd
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-21 20:46:42 -0800

    Create NotFound component

---
+++ client/src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// The page which will display on "Not Found" routes
+export default () => (
+    <div className="wrap">
+        <h2>Not Found</h2>
+        <p>Sorry! We couldn't find the page you're looking for.</p>
+    </div>
+);
