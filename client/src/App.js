e299f6e1b53d9ca406bb3cceb572399d39a6697c
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-21 22:46:54 -0800

    Create the App.js to import our components

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+// Import Components
+import Header from './components/Header';
+import Form from './components/Form';
+import Courses from './components/Courses';
+import CourseDetail from './components/CourseDetail';
+import CreateCourse from './components/CreateCourse';
+import UpdateCourse from './components/UpdateCourse';
+import UserSignIn from './components/UserSignIn';
+import UserSignUp from './components/UserSignUp';
+import UserSignOut from './components/UserSignOut';
+import NotFound from './components/NotFound';
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <ul><li>{ }</li></ul>
-    </div>
-  );
+// Import Context and Private Routes 
+
+
+// Context given to components
+
+
+
+
+//This is our main App component that will build our database through routes 
+const App = () => {
+
 }
 
 export default App;
