0e86057bec153c0e8ebcbe34a83465115245cc13
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-23 01:44:31 -0800

    Adjust spacings

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -4,6 +4,7 @@ import React from 'react';
 export default () => (
     <div className="wrap">
         <h2>Not Found</h2>
+        
         <p>Sorry! We couldn't find the page you're looking for.</p>
     </div>
 );
