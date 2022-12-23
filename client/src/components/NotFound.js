9469bb1ae266a780e70b197b628240b7bea0d4e5
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-23 14:56:33 -0800

    Change NotFound to a functional component

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 // This component will render when a route is not found
-export default () => (
-    <div className="wrap">
-        <h2>Not Found</h2>
-        
-        <p>Sorry! We couldn't find the page you're looking for.</p>
-    </div>
-);
+function NotFound () {
+    return (
+        <div className="wrap">
+            <h2>Not Found</h2>
+            
+            <p>Sorry! We couldn't find the page you're looking for.</p>
+        </div>
+    );
+} 
+
+export default NotFound;
