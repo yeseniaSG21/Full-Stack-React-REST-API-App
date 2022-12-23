d4ea81756ff268945927284130e0781dce60f769
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-23 01:40:48 -0800

    Transform from a class to a function

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,33 +7,34 @@ import { Link } from 'react-router-dom';
     ** Or the user's name and a button for signing out (if there's an authenticated user).
 **/
 
-export default class Header extends React.PureComponent {
-    render() {
-        const { context } = this.props;
-        const authUser = context.authenticatedUser;
-        return (
-            <div className="wrap header--flex">
-                <h1 className="header--logo">
-                    <Link to='/'>Courses</Link>
-                </h1>
-                <nav>
-                    {authUser ? (       // Conditionally Render the Header Nav with authUser as the condition
-                        <React.Fragment>
-                            <ul className='header--signedin'>
-                                <li>Welcome, {authUser.firstName} {authUser.lastName}!</li>
-                                <li><Link to="/signout">Sign Out</Link></li>
-                            </ul>  
-                        </React.Fragment>
-                    ) : (
-                        <React.Fragment>
-                            <ul className='header--signedout'>
-                                <li><Link className="signup" to="/signup">Sign Up</Link></li>
-                                <li><Link className="signin" to="/signin">Sign In</Link></li>
-                            </ul>
-                        </React.Fragment>
-                    )}
-                </nav>
-            </div>
-        );
-    }
+function Header(props) {
+    const { context } = props;
+    const authUser = context.authenticatedUser;
+
+    return (
+        <div className="wrap header--flex">
+            <h1 className="header--logo">
+                <Link to='/'>Courses</Link>
+            </h1>
+            <nav>
+                {authUser ? (       // Conditionally Render the Header Nav with authUser as the condition
+                    <React.Fragment>
+                        <ul className='header--signedin'>
+                            <li>Welcome, {authUser.firstName} {authUser.lastName}!</li>
+                            <li><Link to="/signout">Sign Out</Link></li>
+                        </ul>  
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>
+                        <ul className='header--signedout'>
+                            <li><Link className="signup" to="/signup">Sign Up</Link></li>
+                            <li><Link className="signin" to="/signin">Sign In</Link></li>
+                        </ul>
+                    </React.Fragment>
+                )}
+            </nav>
+        </div>
+    );
 };
+
+export default Header;
