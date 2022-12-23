7ed8a97462ac47fb79c950eea5b947f43b941048
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-22 16:03:07 -0800

    Adjust index.js components

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
