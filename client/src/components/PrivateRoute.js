332cfd522948af2e7977d7850b8c4e191fb135e6
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-22 19:00:25 -0800

    Fix capitalization on redirect

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, redirect } from 'react-router-dom';
 import { Consumer } from '../Context';
 
 /**
@@ -17,7 +17,7 @@ export default ({ component: Component, ...rest }) => {
                     render={props => context.authenticatedUser ? (
                         <Component {...props} />
                     ) : (
-                        <Redirect to={{
+                        <redirect to={{
                             pathname: '/signin',
                             state: { from: props.location }
                         }} />
