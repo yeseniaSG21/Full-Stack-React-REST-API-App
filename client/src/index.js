0104124cdd5d0758f7c40fdd0339a5db92768f20
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-22 19:27:41 -0800

    FIx repetition

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from './Context';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,11 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <Provider>
         <App />
       </Provider>
-    </BrowserRouter>
   </React.StrictMode>
 );
 
